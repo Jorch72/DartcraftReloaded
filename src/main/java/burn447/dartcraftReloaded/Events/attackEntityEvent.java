7969b59813dc5925b04a6c7989960031f9d93ed4
BURN447 <Herobrine098@gmail.com>
2018-08-03 22:10:48 -0700

    Checks for modifiers

--- src/main/java/burn447/dartcraftReloaded/Events/attackEntityEvent.java
+++ src/main/java/burn447/dartcraftReloaded/Events/attackEntityEvent.java
@@ -1,12 +1,16 @@
 package burn447.dartcraftReloaded.Events;
 
 import burn447.dartcraftReloaded.util.MobUtil;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.entity.monster.EntityEnderman;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import static burn447.dartcraftReloaded.Handlers.DCRCapabilityHandler.CAPABILITY_BANE;
 import static burn447.dartcraftReloaded.Handlers.DCRCapabilityHandler.CAPABILITY_PLAYERMOD;
 import static burn447.dartcraftReloaded.Handlers.DCRCapabilityHandler.CAPABILITY_TOOLMOD;
 
@@ -18,28 +22,33 @@ public class attackEntityEvent {
     @SubscribeEvent
     public void AttackEntityEvent(AttackEntityEvent event) {
 
-        if(event.getEntity() instanceof EntityPlayer){
+        int bleedLevel = 0;
+        //Check for Player Attacking Entity
+        if (event.getEntity() instanceof EntityPlayer) {
             EntityPlayer player = event.getEntityPlayer();
-
-            if(player.hasCapability(CAPABILITY_PLAYERMOD, null)){
-
-            }
-        }
-
-
-        if (event.getTarget() instanceof EntityCreeper) {
-            System.out.println("Attacked Creeper");
-            if(event.getEntity() instanceof EntityPlayer) {
-                EntityPlayer player = event.getEntityPlayer();
-                Iterable<ItemStack> armor = player.getArmorInventoryList();
-                for (ItemStack slot : armor) {
-                    if (slot.hasCapability(CAPABILITY_TOOLMOD, null)) {
-                        if (slot.getCapability(CAPABILITY_TOOLMOD, null).hasBane()) {
+            Iterable<ItemStack> armor = player.getArmorInventoryList();
+            //Iterate through Armor
+            for (ItemStack slot : armor) {
+                //Only need one Level of Bane
+                if (slot.hasCapability(CAPABILITY_TOOLMOD, null)) {
+                    if (slot.getCapability(CAPABILITY_TOOLMOD, null).hasBane()) {
+                        if (event.getTarget() instanceof EntityCreeper) {
                             MobUtil.removeCreeperExplodeAI(((EntityCreeper) event.getTarget()));
+                        }
+                        else if(event.getTarget() instanceof EntityEnderman){
+                            event.getTarget().getCapability(CAPABILITY_BANE, null).setTeleportAbility(false);
                         }
                     }
                 }
+
+                if(slot.getCapability(CAPABILITY_TOOLMOD, null).hasBleeding(1))
+                    bleedLevel++;
+
+
             }
+
+            //Bleeding
+            MobUtil.addBleedingEffect(bleedLevel, (EntityLivingBase) event.getTarget());
         }
     }
 }
